d3945d76ab7e79b212d4d541cce902f5e412a548
Yingtong Chen <yingtc1996@gmail.com>
2022-10-16 00:46:58 -0700

    Ignore DS_Store file and display FPS

--- reduce_resolution.py
+++ reduce_resolution.py
@@ -1,15 +1,22 @@
 import cv2
 import numpy as np
 
-TARGET_SIZE = (640, 360)
-TARGET_FPS = 20 # It might be complicated to use non-divisible number from target
+TARGET_SIZE = (1280, 720)
+# TARGET_FPS = 60 # It might be complicated to use non-divisible number from target
+
+# TARGET_SIZE = (640, 360)
+TARGET_FPS = 60 # It might be complicated to use non-divisible number from target
 
 # TARGET_SIZE = (7680, 4320)
 # TARGET_FPS = 60 # It might be complicated to use non-divisible number from target
 
 def reduce_quality(input_file, output_file):
+	print(f"Parsing {input_file}")
+
 	cap = cv2.VideoCapture(input_file)
 	fourcc = cv2.VideoWriter_fourcc(*'avc1') # Apple's version of MPEG4 part 10 / H.264
+
+	print(f"	Video FPS: {cap.get(cv2.CAP_PROP_FPS)}")
 
 	out = cv2.VideoWriter(output_file, fourcc, TARGET_FPS, TARGET_SIZE)
 
@@ -25,8 +32,9 @@ def reduce_quality(input_file, output_file):
 			# If we want to allow created video to contain slow motion, we need to 
 			#   comment out this extra rate while maintaining reduced frame rate
 			#   comparing to source
-			ret, frame = cap.read()
-			ret, frame = cap.read()
+			if (TARGET_FPS != 60):
+				ret, frame = cap.read()
+				ret, frame = cap.read()
 
 			###################
 			# Debug only
@@ -52,10 +60,8 @@ if (len(sys.argv) > 2):
 	OUTPUT_FOLDER = sys.argv[2]
 
 	for file in os.listdir(INPUT_FOLDER):
-		# reduce_quality(input_file=f"./{file}", output_file=f"{OUTPUT_FOLDER/shortened_{file}}")
-		print(f"./{file}", f"{OUTPUT_FOLDER}/shortened_{file}")
-
-		# reduce_quality(input_file='./target_short.MOV', output_file='./output.MOV')
+		if (file != ".DS_Store" or (len(file) > 1 and file[0] != '.')): # Do not take cache or hidden file into consideration
+			reduce_quality(input_file=f"./{INPUT_FOLDER}/{file}", output_file=f"./{OUTPUT_FOLDER}/shortened_{file}")
 else:
 	print(f"Not sure what to do with: {sys.argv}")
 
